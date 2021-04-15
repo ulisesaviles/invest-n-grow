80a783b49b40bde311b3b23f37b13ee8ac3dcbb0
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-04-14 20:31:22 -0700

    Progress indicator is done

--- config/colors.js
+++ config/colors.js
@@ -1,16 +1,22 @@
 export default {
   dark: {
+    arrow: "rgb(255,255,255)",
     background: "rgb(0,0,0)",
+    barBackground: "rgb(50, 50, 50)",
+    boxShadow: "rgba(0,0,0,0.8)",
+    containers: "rgb(35, 35, 35)",
     fonts: {
       primary: "rgb(255, 255, 255)",
     },
-    containers: "rgb(35, 35, 35)",
   },
   light: {
+    arrow: "rgb(50,50,50)",
     background: "rgb(240,240,240)",
+    barBackground: "rgb(240, 240, 240)",
+    boxShadow: "rgba(0,0,0,0.8)",
+    containers: "rgb(255, 255, 255)",
     fonts: {
       primary: "rgb(0,0,0)",
     },
-    containers: "rgb(255, 255, 255)",
   },
 };
