c2603d93d10050f90e29efd083e5b4b05c0615b3
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-04-15 20:34:40 -0700

    ProgresI: Added animation, acces to control values

--- components/ProgressIndicator.js
+++ components/ProgressIndicator.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Animated } from "react-native";
 import { useColorScheme } from "react-native-appearance";
 
 import colors from "../config/colors";
 
 import { LinearGradient } from "expo-linear-gradient";
 
-export default ProgressIndicator = () => {
+export default ProgressIndicator = (props) => {
   let colorScheme = useColorScheme();
+  const animatedValues = {
+    lifeQuality: useRef(new Animated.Value(0)).current,
+    herritage: useRef(new Animated.Value(0)).current,
+    debt: useRef(new Animated.Value(0)).current,
+  };
 
   const styles = StyleSheet.create({
-    bar: {
+    progressBar: {
       width: "100%",
       height: "100%",
       borderRadius: 7,
     },
-    barContainer: {
+    progressBarContainer: {
       height: 13,
       width: "100%",
       borderRadius: 20,
@@ -28,7 +33,7 @@ export default ProgressIndicator = () => {
       },
       shadowOpacity: 0.16,
     },
-    container: {
+    progressContainer: {
       backgroundColor: colors[colorScheme].containers,
       borderRadius: 20,
       width: "95%",
@@ -42,13 +47,13 @@ export default ProgressIndicator = () => {
       },
       shadowOpacity: 0.16,
     },
-    group: {
+    progressGroup: {
       marginVertical: 4,
     },
-    text: {
+    progressText: {
       color: colors[colorScheme].fonts.primary,
     },
-    textContainer: {
+    progressTextContainer: {
       flexDirection: "row",
       justifyContent: "space-between",
       marginBottom: 2,
@@ -56,53 +61,57 @@ export default ProgressIndicator = () => {
   });
 
   return (
-    <View style={styles.container}>
-      <View style={styles.group}>
-        <View style={styles.textContainer}>
-          <Text style={[styles.text, { fontWeight: "700" }]}>Life Quality</Text>
-          <Text style={styles.text}>100pts</Text>
+    <View style={styles.progressContainer}>
+      <View style={styles.progressGroup}>
+        <View style={styles.progressTextContainer}>
+          <Text style={[styles.progressText, { fontWeight: "700" }]}>
+            Life Quality
+          </Text>
+          <Text style={styles.progressText}>100pts</Text>
         </View>
-        <View style={styles.barContainer}>
+        <View style={styles.progressBarContainer}>
           <LinearGradient
             colors={[
               colors[colorScheme].gradients.blue.start,
               colors[colorScheme].gradients.blue.end,
             ]}
-            style={[styles.bar, { width: "80%" }]}
+            style={[styles.progressBar, { width: "80%" }]}
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 1 }}
           />
         </View>
       </View>
-      <View style={styles.group}>
-        <View style={styles.textContainer}>
-          <Text style={[styles.text, { fontWeight: "700" }]}>Herritage</Text>
-          <Text style={styles.text}>$10M</Text>
+      <View style={styles.progressGroup}>
+        <View style={styles.progressTextContainer}>
+          <Text style={[styles.progressText, { fontWeight: "700" }]}>
+            Herritage
+          </Text>
+          <Text style={styles.progressText}>$10M</Text>
         </View>
-        <View style={styles.barContainer}>
+        <View style={styles.progressBarContainer}>
           <LinearGradient
             colors={[
               colors[colorScheme].gradients.green.start,
               colors[colorScheme].gradients.green.end,
             ]}
-            style={[styles.bar, { width: "50%" }]}
+            style={[styles.progressBar, { width: "50%" }]}
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 1 }}
           />
         </View>
       </View>
-      <View style={styles.group}>
-        <View style={styles.textContainer}>
-          <Text style={[styles.text, { fontWeight: "700" }]}>Debt</Text>
-          <Text style={styles.text}>$10M</Text>
+      <View style={styles.progressGroup}>
+        <View style={styles.progressTextContainer}>
+          <Text style={[styles.progressText, { fontWeight: "700" }]}>Debt</Text>
+          <Text style={styles.progressText}>$10M</Text>
         </View>
-        <View style={styles.barContainer}>
+        <View style={styles.progressBarContainer}>
           <LinearGradient
             colors={[
               colors[colorScheme].gradients.red.start,
               colors[colorScheme].gradients.red.end,
             ]}
-            style={[styles.bar, { width: "70%" }]}
+            style={[styles.progressBar, { width: "70%" }]}
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 1 }}
           />
--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,6 +6,7 @@ import {
   SafeAreaView,
   Dimensions,
   TouchableOpacity,
+  Animated,
 } from "react-native";
 import { useColorScheme } from "react-native-appearance";
 
@@ -16,6 +17,30 @@ import { ProgressIndicator, Properties, Assets } from "../components";
 
 export default Home = () => {
   let colorScheme = useColorScheme();
+  const [firstload, setFirtsLoad] = useState(true);
+
+  const animatedValues = {
+    lifeQuality: useRef(new Animated.Value(0)).current,
+    herritage: useRef(new Animated.Value(0)).current,
+    debt: useRef(new Animated.Value(0)).current,
+  };
+
+  const updateIndicators = (indicator, value) => {
+    Animated.timing(animatedValues[indicator], {
+      toValue: (Dimensions.get("screen").width * 0.95 - 40) * (value / 100),
+      duration: 400,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  if (firstload) {
+    setFirtsLoad(false);
+    setTimeout(() => {
+      updateIndicators("lifeQuality", 80);
+      updateIndicators("herritage", 50);
+      updateIndicators("debt", 70);
+    }, 300);
+  }
 
   const styles = StyleSheet.create({
     assetsContainer: {
@@ -50,6 +75,49 @@ export default Home = () => {
       position: "relative",
       width: "100%",
       alignItems: "center",
+    },
+    progressBar: {
+      width: "100%",
+      height: "100%",
+      borderRadius: 7,
+    },
+    progressBarContainer: {
+      height: 13,
+      width: "100%",
+      borderRadius: 20,
+      backgroundColor: colors[colorScheme].barBackground,
+      shadowColor: colors[colorScheme].boxShadow,
+      shadowRadius: 4,
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowOpacity: 0.16,
+    },
+    progressContainer: {
+      backgroundColor: colors[colorScheme].containers,
+      borderRadius: 20,
+      width: "95%",
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      shadowColor: colors[colorScheme].boxShadow,
+      shadowRadius: 4,
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowOpacity: 0.16,
+    },
+    progressGroup: {
+      marginVertical: 4,
+    },
+    progressText: {
+      color: colors[colorScheme].fonts.primary,
+    },
+    progressTextContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginBottom: 2,
     },
     properties: {
       position: "absolute",
@@ -92,7 +160,83 @@ export default Home = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.progressIndicator}>
-        <ProgressIndicator />
+        <View style={styles.progressContainer}>
+          <View style={styles.progressGroup}>
+            <View style={styles.progressTextContainer}>
+              <Text style={[styles.progressText, { fontWeight: "700" }]}>
+                Life Quality
+              </Text>
+              <Text style={styles.progressText}>100pts</Text>
+            </View>
+            <View style={styles.progressBarContainer}>
+              <Animated.View
+                style={{
+                  width: animatedValues.lifeQuality,
+                }}
+              >
+                <LinearGradient
+                  colors={[
+                    colors[colorScheme].gradients.blue.start,
+                    colors[colorScheme].gradients.blue.end,
+                  ]}
+                  style={[styles.progressBar, { width: "100%" }]}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 1 }}
+                />
+              </Animated.View>
+            </View>
+          </View>
+          <View style={styles.progressGroup}>
+            <View style={styles.progressTextContainer}>
+              <Text style={[styles.progressText, { fontWeight: "700" }]}>
+                Herritage
+              </Text>
+              <Text style={styles.progressText}>$10M</Text>
+            </View>
+            <View style={styles.progressBarContainer}>
+              <Animated.View
+                style={{
+                  width: animatedValues.herritage,
+                }}
+              >
+                <LinearGradient
+                  colors={[
+                    colors[colorScheme].gradients.green.start,
+                    colors[colorScheme].gradients.green.end,
+                  ]}
+                  style={[styles.progressBar, { width: "100%" }]}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 1 }}
+                />
+              </Animated.View>
+            </View>
+          </View>
+          <View style={styles.progressGroup}>
+            <View style={styles.progressTextContainer}>
+              <Text style={[styles.progressText, { fontWeight: "700" }]}>
+                Debt
+              </Text>
+              <Text style={styles.progressText}>$10M</Text>
+            </View>
+            <View style={styles.progressBarContainer}>
+              <Animated.View
+                style={{
+                  width: animatedValues.debt,
+                }}
+              >
+                <LinearGradient
+                  colors={[
+                    colors[colorScheme].gradients.red.start,
+                    colors[colorScheme].gradients.red.end,
+                  ]}
+                  style={[styles.progressBar, { width: "100%" }]}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 1 }}
+                />
+              </Animated.View>
+            </View>
+          </View>
+        </View>
       </View>
       <View style={styles.properties}>
         <Properties />
