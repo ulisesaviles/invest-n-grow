d0a2ca895930c3fb881a6b85c71df91c8afdafcc
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-04-15 18:26:22 -0700

    Added Assets

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 import ProgressIndicator_ from "./ProgressIndicator";
 import Properties_ from "./Properties";
+import Assets_ from "./Assets";
 
 export const ProgressIndicator = ProgressIndicator_;
 export const Properties = Properties_;
+export const Assets = Assets_;
