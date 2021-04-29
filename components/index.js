f48d255997fb041df8f4b87c38fe29a4ccb730af
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-04-29 16:21:32 -0700

    Deleted unnecesary files

--- components/index.js
+++ components/index.js
@@ -1,7 +1,3 @@
-import ProgressIndicator_ from "./ProgressIndicator";
 import Properties_ from "./Properties";
-import Assets_ from "./Assets";
 
-export const ProgressIndicator = ProgressIndicator_;
 export const Properties = Properties_;
-export const Assets = Assets_;
