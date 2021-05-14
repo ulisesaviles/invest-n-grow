915f657b8bd6d29d385469e70ddfea577e474071
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-05-13 18:52:32 -0700

    Imported myProperties

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
 import Properties_ from "./Properties";
 import Event_ from "./Event";
 import Progress_ from "./ProgressIndicators";
+import MyProperties_ from "./MyProperties";
 
 export const Properties = Properties_;
 export const Event = Event_;
 export const ProgressIndicators = Progress_;
+export const MyProperties = MyProperties_;
