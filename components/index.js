d0168002657d8ba137f8c340581f6a1fd88387ab
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-05-13 18:40:10 -0700

    Added progress indicators

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 import Properties_ from "./Properties";
 import Event_ from "./Event";
+import Progress_ from "./ProgressIndicators";
 
 export const Properties = Properties_;
 export const Event = Event_;
+export const ProgressIndicators = Progress_;
