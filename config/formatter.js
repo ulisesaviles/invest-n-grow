6cff696fe16e2ed400d44d1cdbff3f4f4bceb767
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-05-17 10:27:15 -0700

    Formats the price for negative numbres

--- config/formatter.js
+++ config/formatter.js
@@ -1,13 +1,27 @@
 export const priceToStr = (price) => {
-  price = price.toString();
+  if (price > 0) {
+    price = price.toString();
 
-  if (price.length < 4) {
-    return price;
+    if (price.length < 4) {
+      return price;
+    }
+
+    if (price.length < 7) {
+      return price.slice(0, price.length - 3) + "k";
+    }
+
+    return price.slice(0, price.length - 6) + "M";
+  } else if (price < 0) {
+    price = price.toString();
+
+    if (price.length < 5) {
+      return price;
+    }
+
+    if (price.length < 8) {
+      return price.slice(0, price.length - 3) + "k";
+    }
+
+    return price.slice(0, price.length - 6) + "M";
   }
-
-  if (price.length < 7) {
-    return price.slice(0, price.length - 3) + "k";
-  }
-
-  return price.slice(0, price.length - 6) + "M";
 };
