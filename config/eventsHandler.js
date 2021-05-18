6e381aac444c576966a76cceffe6e49e5a063bb0
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-05-18 14:36:12 -0700

    Bug fixed and added cashflow

--- config/eventsHandler.js
+++ config/eventsHandler.js
@@ -1,29 +1,77 @@
 import events from "./events.json";
 import store from "./redux/store";
+import properties from "./properties";
 
-export const getEvent = () => {
+export const newEvent = () => {
   const eventWName = (name) => {
     for (let i = 0; i < events.length; i++) {
       if (events[i].name === name) {
-        console.log(events[i]);
         return events[i];
       }
     }
     return null;
   };
 
+  const getCashFlow = () => {
+    let currentState = store.getState();
+    let res = 0;
+    for (let i = 0; i < currentState.currentGame.ownedProperties.length; i++) {
+      if (
+        propertyWName(currentState.currentGame.ownedProperties[i].name).type ===
+          "cars" ||
+        propertyWName(currentState.currentGame.ownedProperties[i].name).type ===
+          "cash" ||
+        propertyWName(currentState.currentGame.ownedProperties[i].name).type ===
+          "crypto" ||
+        propertyWName(currentState.currentGame.ownedProperties[i].name).type ===
+          "realEstate" ||
+        propertyWName(currentState.currentGame.ownedProperties[i].name).type ===
+          "stocks"
+      ) {
+        res +=
+          propertyWName(currentState.currentGame.ownedProperties[i].name).stats[
+            currentState.currentGame.ownedProperties[i].isAnAsset
+              ? "asset"
+              : "commodity"
+          ].cashFlow *
+          currentState.currentGame.multipliers[
+            propertyWName(currentState.currentGame.ownedProperties[i].name).type
+          ];
+      } else {
+        res += propertyWName(currentState.currentGame.ownedProperties[i].name)
+          .stats[
+          currentState.currentGame.ownedProperties[i].isAnAsset
+            ? "asset"
+            : "commodity"
+        ].cashFlow;
+      }
+    }
+    console.log(res);
+    return res;
+  };
+
   const newValidEvent = () => {
     let randomIndex;
     while (true) {
       randomIndex = randomIndexInRange();
+      console.log(randomIndex);
       if (!events[randomIndex].onlyOnSequence) {
         return events[randomIndex];
       }
     }
   };
 
+  const propertyWName = (name) => {
+    for (let i = 0; i < properties.length; i++) {
+      if (properties[i].name === name) {
+        return properties[i];
+      }
+    }
+    return null;
+  };
+
   const randomIndexInRange = () => {
-    return Math.floor(Math.random() * events.length - 1);
+    return Math.floor(Math.random() * (events.length - 1));
   };
 
   const currentState = store.getState();
@@ -32,6 +80,7 @@ export const getEvent = () => {
       type: "newEvent",
       payload: {
         newEvent: newValidEvent(),
+        cashFlow: getCashFlow(),
       },
     });
   } else {
@@ -40,6 +89,7 @@ export const getEvent = () => {
         type: "newEvent",
         payload: {
           newEvent: newValidEvent(),
+          cashFlow: getCashFlow(),
         },
       });
     } else {
@@ -47,6 +97,7 @@ export const getEvent = () => {
         type: "newEvent",
         payload: {
           newEvent: eventWName(currentState.currentGame.passedEvents[0].next),
+          cashFlow: getCashFlow(),
         },
       });
     }
